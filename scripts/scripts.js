9789beab30684740d4597b4308aaa1858b314667
Kurakina Anastasia <anastasiya.kurakina@zazmic.com>
2023-06-18 14:23:50 +0300

    Add dynamic event listener

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,22 @@ document.getElementById('selectedProduct')
             }]
         });
 });
+
+
+document.getElementById('main-block')
+    .addEventListener('click', (event) => {
+        const el = event.target.className.includes('product-card')
+            ? event.target
+            : event.target.closest('.product-card');
+        if (el) {
+            const productName = el.id;
+            dataLayer.push({
+                'event': 'CoffeTypeClicked',
+                'value': productName,
+                'items' : [{
+                    'id': '1234',
+                    'google_business_vertical': productName
+                }]
+            });
+        }
+    });
